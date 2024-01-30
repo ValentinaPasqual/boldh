f844ecc1efc372e1edf756970c07de7608b576ae
Valentina Pasqual <valentina.pasqual@studio.unibo.it>
2024-01-30 15:09:25 +0100

    git actions for editor fetch

--- update_sheet.py
+++ update_sheet.py
@@ -2,6 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import requests
+import json
 
 # Load credentials and authorize the Google Sheets API
 scope = ['https://spreadsheets.google.com/feeds']
@@ -10,25 +11,48 @@ client = gspread.authorize(creds)
 
 # Open the Google Sheet using its ID
 sheet_id = '1WKRWIkQ5qqr5caCEl5yaeHHuzDv_yF0fGpQs9dvBTgk'
-sheet = client.open_by_key(sheet_id).sheet1
+document = client.open_by_key(sheet_id)
 
-# Get all values from the sheet
-values = sheet.get_all_values()
+def agenda_json_builder(json_element):
+    print(json_element)
 
-# Assuming your sheet has a header row, extract column names
-headers = values[0]
+    return json_element
 
-# Create a list of dictionaries representing each row
-news_data = [dict(zip(headers, row)) for row in values[1:]]
 
-# Call the fetch_news function to get the actual news data
-news_data = fetch_news()
+def get_data_from_sheet(document, sheet, file_name):
 
-# Convert data to DataFrame
-df = pd.DataFrame(news_data)
+    # Get data from the chosen sheet
+    values = sheet.get_all_values()
+
+    # Assuming your sheet has a header row, extract column names
+    headers = values[0]
+
+    # Create a list of dictionaries representing each row
+    data = [dict(zip(headers, row)) for row in values[1:]]
+
+    # Convert data to DataFrame
+    df = pd.DataFrame(data)
+
+    # Convert DataFrame to list of dictionaries
+    output_list = df.to_dict(orient='records')
+
+    # Convert list to JSON format
+    json_el = json.dumps(output_list, indent=2)
+
+    if 'agenda' in file_name:
+        json_el = agenda_json_builder(json_el)
+
+    with open(f'content/{file_name}', 'w') as file:
+        file.write(json_el)
+
+news = document.get_worksheet(0)
+get_data_from_sheet(document, news, 'news.json')
+
+agenda = document.get_worksheet(1)
+get_data_from_sheet(document, agenda, 'agenda.json')
 
 # Clear existing data in the Google Sheet
-sheet.clear()
+#sheet.clear()
 
 # Update the Google Sheet with the new data
-sheet.update([df.columns.values.tolist()] + df.values.tolist())
+#sheet.update([df.columns.values.tolist()] + df.values.tolist())
